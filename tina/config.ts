d7bd5a896108095e1e60f443be034ffcfc68c5fc
swilsonify <saraidwilson@gmail.com>
2025-10-08 14:46:34 -0400

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -2,26 +2,25 @@ import { defineConfig } from "tinacms";
 
 export default defineConfig({
   branch: "main",
-clientId: "29614480-677f-4dd1-a969-a2e5ffdd191e",
+  clientId: "29614480-677f-4dd1-a969-a2e5ffdd191e",
   token: "9fc63b6a4b245e22501e182dfdd1b0c7829f9296",
-, // Get this from tina.io
   
   build: {
     outputFolder: "admin",
-    publicFolder: "/",
+    publicFolder: ".",
   },
   
   media: {
     tina: {
       mediaRoot: "assets/images",
-      publicFolder: "/",
+      publicFolder: ".",
     },
   },
   
   schema: {
     collections: [
       {
-        name: "page",
+        name: "pages",
         label: "Pages",
         path: "content/pages",
         format: "md",
